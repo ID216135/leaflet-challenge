1918ad1ffa8e4e6f1fc12ca03cae55fc3582223b
Ian Denker <igd3@case.edu>
2024-01-11 23:57:38 -0500

    assignment complete

---
+++ static/js/logic.js
@@ -0,0 +1,69 @@
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function.
+    createFeatures(data.features);
+  });
+    function getColor(d) {
+        return d > 90 ? '#FF5533' :
+               d > 70  ? '#FF9333' :
+               d > 50  ? '#FFB533' :
+               d > 30  ? '#FFE333' :
+               d > 10   ? '#D1FF33' :
+               d > -10   ? '#BBFF33' :
+                          '#8AFF33';}  
+  function createFeatures(earthquakeData) {
+    
+
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    let earthquakes = L.geoJSON(earthquakeData, {pointToLayer: (feature,latlng) => {
+        return L.circle(latlng,{fillOpacity: 0.75,color: getColor(feature.geometry.coordinates[2]),
+        fillColor: getColor(feature.geometry.coordinates[2]), radius: feature.properties.mag*15000}
+        ).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p>`)
+    }})
+    
+    ;
+  
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+  }
+  
+  function createMap(earthquakes) {
+  
+    // Create the base layers.
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+    let myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [street, earthquakes]
+      
+    });
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        depths = [-10, 10, 30, 50, 70, 90]
+
+    // loop through our earthquake depth intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < depths.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
+            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+    };
+
+    legend.addTo(myMap);
+    
+
+  }
+  
